173d9e9590beb1aea0cae32d6695b4a5b2e31967
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-05-23 22:08:56 +0300

    logging with color

---
+++ green_city/tests/api/get_source_countent_test.py
@@ -0,0 +1,9 @@
+import  green_city.src.logging_config
+import requests
+from green_city.src.config import API_BASE_URL_8085, ENDPOINTS
+import logging
+from jsonschema import validate
+
+logger = logging.getLogger(__name__)
+
+
