62d650ceb07c13aceb05f10f149cfbc8712d5b07
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-05-23 23:04:08 +0300

    implementation of test_get_summary_by_id_unauthorized()

--- green_city/tests/api/get_source_countent_test.py
+++ green_city/tests/api/get_source_countent_test.py
@@ -14,10 +14,27 @@ summary_schema = {
     "required": ["message"],
     "additionalProperties": False
 }
+
+summary_schema_error = {
+  "type": "object",
+    "properties": {
+        "timestamp": {"type": "string"},
+        "status": {"type": "number"},
+        "error": {"type": "string"},
+        "path": {"type": "string"}
+    },
+    "required": ["timestamp", "status", "error", "path"],
+    "additionalProperties": False
+}
     
 data = {
     "404_response_body":{
         "message":"Eco new doesn't exist by this id: 101"
+    },
+    "401_response_body":{
+        "status": 401,
+        "error": "Unauthorized",
+        "path": "/eco-news/1/summary"
     }
 }
     
@@ -32,3 +49,15 @@ def test_get_summary_by_id_not_found(auth_token):
     expected_status_code = 404
     assert response.status_code == expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}"
     assert response.json()["message"]==data["404_response_body"].get("message")
+
+def test_get_summary_by_id_unauthorized():
+    news_id = 1
+    response = requests.get(
+        f'{API_BASE_URL_8085}{ENDPOINTS["summary"].format(news_id)}',
+        headers={"Authorization": ''},
+    )
+    logger.info(f"Status code: {response.status_code}")
+    validate(instance=response.json(), schema=summary_schema_error)
+    expected_status_code = 401
+    assert response.status_code == expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}"
+    assert response.json()["error"]==data["401_response_body"].get("error")
