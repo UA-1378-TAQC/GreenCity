9683c23adfe43efb1b8a2abdcf2449b062760d86
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-05-23 23:08:00 +0300

    a little update

--- green_city/tests/api/get_source_countent_test.py
+++ green_city/tests/api/get_source_countent_test.py
@@ -15,14 +15,21 @@ summary_schema = {
     "additionalProperties": False
 }
     
+data = {
+    "404_response_body":{
+        "message":"Eco new doesn't exist by this id: 101"
+    }
+}
+    
 def test_get_summary_by_id_not_found(auth_token):
     news_invalid_id = 101
     response = requests.get(
         f'{API_BASE_URL_8085}{ENDPOINTS["summary"].format(news_invalid_id)}',
         headers={"Authorization": auth_token},
     )
-    logger.info(response.status_code)
+    logger.info(f"Status code: {response.status_code}")
     validate(instance=response.json(), schema=summary_schema)
-    assert response.status_code == 404, f"Expected status code 404, but got {response.status_code}"
-    assert response.json()["message"]=="Eco new doesn't exist by this id: 101"
+    expected_status_code = 404
+    assert response.status_code == expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}"
+    assert response.json()["message"]==data["404_response_body"].get("message")
 
