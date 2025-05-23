0e3f17eed84990ff451f933c0a3bc0cd25352e48
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-05-23 22:43:17 +0300

    implementation of test_get_summary_by_id_success()

--- green_city/tests/api/get_source_countent_test.py
+++ green_city/tests/api/get_source_countent_test.py
@@ -6,13 +6,33 @@ from jsonschema import validate
 
 logger = logging.getLogger(__name__)
 
-summary_schema = {
+summary_schema_message = {
   "type": "object",
     "properties": {
         "message": {"type": "string"}
     },
     "required": ["message"],
     "additionalProperties": False
+}
+
+summary_schema_valid_result = {
+  "type": "object",
+    "properties": {
+        "content": {"type": "string"},
+        "source": {"type": "string"}
+    },
+    "required": ["content","source"],
+    "additionalProperties": False
+}
+
+data = {
+    "200_response_body":{
+        "content": "<p>My Test Eco News My Test Eco News</p>",
+        "source": ""
+    },
+    "404_response_body":{
+        "message":"Eco new doesn't exist by this id: 101"
+    }
 }
     
 def test_get_summary_by_id_not_found(auth_token):
@@ -21,8 +41,21 @@ def test_get_summary_by_id_not_found(auth_token):
         f'{API_BASE_URL_8085}{ENDPOINTS["summary"].format(news_invalid_id)}',
         headers={"Authorization": auth_token},
     )
-    logger.info(response.status_code)
+    logger.info(f"Status code: {response.status_code}")
     validate(instance=response.json(), schema=summary_schema)
-    assert response.status_code == 404, f"Expected status code 404, but got {response.status_code}"
-    assert response.json()["message"]=="Eco new doesn't exist by this id: 101"
+    expected_status_code = 404
+    assert response.status_code == expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}"
+    assert response.json()["message"]==data["404_response_body"].get("message")
+
+def test_get_summary_by_id_success(auth_token):
+    news_id = 1
+    response = requests.get(
+        f'{API_BASE_URL_8085}{ENDPOINTS["summary"].format(news_id)}',
+        headers={"Authorization": auth_token},
+    )
+    logger.info(f"Status code: {response.status_code}")
+    validate(instance=response.json(), schema=summary_schema_valid_result)
+    expected_status_code = 200
+    assert response.status_code == expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}"
+    assert response.json()["content"]==data["200_response_body"].get("content")
 
