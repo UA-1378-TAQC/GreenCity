183e6125a88a0904237b5f211c0aadb58a78fc39
Dmytro Zubenko <fokist@outlook.com>
2025-05-23 23:08:27 +0300

    get_recommended_news test added using fixtures and jsonschema validation

--- green_city/tests/api/conftest.py
+++ green_city/tests/api/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 import requests
+import json
 from green_city.src.config import TEST_USER_EMAIL, TEST_USER_PASSWORD, SECRET_KEY
-from green_city.src.config import API_BASE_URL_8065, ENDPOINTS
+from green_city.src.config import API_BASE_URL_8065, API_BASE_URL_8085, ENDPOINTS
 
-@pytest.fixture(scope="session")
+
+@pytest.fixture(scope="module")
 def login_token():
     login_data = {
         "email": TEST_USER_EMAIL,
@@ -16,6 +18,7 @@ def login_token():
     assert token is not None, "Token not found in response"
     return token
 
+
 @pytest.fixture
 def auth_token(login_token):
     return f"Bearer {login_token}"
@@ -23,11 +26,49 @@ def auth_token(login_token):
 
 @pytest.fixture
 def create_comment(auth_token):
-    print("Comment creation...")
     data = '{"text": "{comment text here}", "parentCommentId": 0}'
     files = {'request': (None, data)}
-
     yield files
-    print("Place comment deleted logic below")
 
+
+@pytest.fixture(scope="function")
+def create_news(auth_token):
+    url = f"{API_BASE_URL_8085}{ENDPOINTS['create_eco_news']}"
+
+    dto_request = {
+        "image": "string",
+        "source": "https://example.org/",
+        "shortInfo": "string",
+        "title": "Some OMEGA cool title here!!!",
+        "text": "Some cool text here!!!",
+        "tags": ["news"],
+        "titleTranslation": {
+            "content": "string",
+            "languageCode": "string"
+        },
+        "textTranslation": {
+            "content": "string",
+            "languageCode": "string"
+        }
+    }
+
+    dto_request_str = json.dumps(dto_request)
+
+    files = {
+        'addEcoNewsDtoRequest': (None, dto_request_str),
+        'image': (None, '')
+    }
+
+    headers = {
+        "Authorization": auth_token
+    }
+
+    response = requests.post(url, headers=headers, files=files)
+
+    news_id = response.json().get("id")
+    yield news_id
+
+    delete_url = f"{API_BASE_URL_8085}{ENDPOINTS['delete_eco_news'].format(news_id)}"
+    del_response = requests.delete(delete_url, headers=headers)
+    assert del_response.status_code == 200, "Failed to delete news"
 
