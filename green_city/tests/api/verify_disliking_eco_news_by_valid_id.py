843f999b22b492669881fd18e0231f448d76def7
Dmytro Zubenko <fokist@outlook.com>
2025-05-24 18:29:07 +0300

    refactored fixtures

--- green_city/tests/api/verify_disliking_eco_news_by_valid_id.py
+++ green_city/tests/api/verify_disliking_eco_news_by_valid_id.py
@@ -1,8 +1,10 @@
 import requests
 from green_city.src.config import API_BASE_URL_8085, ENDPOINTS
+from green_city.tests.api.fixtures.conftest import *
 
 def test_dislike_news(create_news,auth_token_second_user):
     news_id = create_news
+    #todo like news here
     full_url = f"{API_BASE_URL_8085}{ENDPOINTS['dislike_eco_news'].format(news_id)}"
     headers = {
         "Authorization": auth_token_second_user
@@ -11,4 +13,3 @@ def test_dislike_news(create_news,auth_token_second_user):
     response = requests.post(full_url, headers=headers)
 
     assert response.status_code == 200, "Failed to dislike selected news"
-
