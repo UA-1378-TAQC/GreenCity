bdee5ae88180539ec55d7e12ca418abe457d699b
Dmytro Zubenko <fokist@outlook.com>
2025-05-24 21:13:24 +0300

    redundant method removal

--- green_city/tests/api/fixtures/login_fixture.py
+++ green_city/tests/api/fixtures/login_fixture.py
@@ -1,6 +1,4 @@
 import pytest
-import requests
-import json
 from green_city.src.util.auth_helpers import get_auth_token
 from green_city.src.config import API_BASE_URL_8065, API_BASE_URL_8085, ENDPOINTS, TEST_USER_EMAIL, TEST_USER_PASSWORD, CREATOR_USER_EMAIL, CREATOR_USER_PASSWORD
 
@@ -11,14 +9,4 @@ def auth_token():
 @pytest.fixture(scope="session")
 def auth_token_second_user():
     return get_auth_token(CREATOR_USER_EMAIL, CREATOR_USER_PASSWORD)
-
-@pytest.fixture
-def create_comment(auth_token):
-    data = '{"text": "{comment text here}", "parentCommentId": 0}'
-    files = {'request': (None, data)}
-    yield files
-
-
-
-
 
