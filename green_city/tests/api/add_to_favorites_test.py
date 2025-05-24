7f74f2b6671fb8ec818bd10d378b411f4595ed1f
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-05-25 02:33:45 +0300

    refactoring

--- green_city/tests/api/add_to_favorites_test.py
+++ green_city/tests/api/add_to_favorites_test.py
@@ -1,5 +1,4 @@
 import green_city.src.util.logging_config
-import pytest
 import requests
 from green_city.src.config import API_BASE_URL_8085, ENDPOINTS
 import logging
@@ -8,13 +7,16 @@ from ..data.schema.all_news_schema import schema_all_news
 
 logger = logging.getLogger(__name__)
 
+def request_add_to_favorites(token, id):
+    return requests.post(
+      f'{API_BASE_URL_8085}{ENDPOINTS["favorites"].format(id)}',
+      headers={"Authorization": token}
+    )
+
 def test_add_to_favorites_success(create_news, auth_token):
     id = create_news
     logger.info(f'Request to add eco-new with id={id} to favorites')
-    response = requests.post(
-        f'{API_BASE_URL_8085}{ENDPOINTS["favorites"].format(id)}',
-        headers={"Authorization": auth_token}
-    )
+    response = request_add_to_favorites(auth_token, id)
     logger.info(f"Status code: {response.status_code}")
     expected_status_code = 200
     assert response.status_code == expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}"
@@ -31,5 +33,3 @@ def test_add_to_favorites_success(create_news, auth_token):
     validate(instance=data, schema=schema_all_news)
     matching_users = next((entry for entry in data["page"] if entry["id"] == id), None)
     assert matching_users['favorite'] == True, f'Expected "favorite": True, but was {matching_users["favorite"]}'
-    
-    
