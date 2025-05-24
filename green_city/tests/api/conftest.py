7b8128c01f12fad5fcfa6489ccb0986f110a0a8a
Dmytro Zubenko <fokist@outlook.com>
2025-05-24 17:33:50 +0300

    merge conflict solved

--- green_city/tests/api/conftest.py
+++ green_city/tests/api/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 import requests
+import json
 from green_city.src.util.auth_helpers import get_auth_token
-from green_city.src.config import TEST_USER_EMAIL, TEST_USER_PASSWORD, CREATOR_USER_EMAIL, CREATOR_USER_PASSWORD
+from green_city.src.config import API_BASE_URL_8065, API_BASE_URL_8085, ENDPOINTS, TEST_USER_EMAIL, TEST_USER_PASSWORD, CREATOR_USER_EMAIL, CREATOR_USER_PASSWORD
 
 @pytest.fixture(scope="session")
 def auth_token():
