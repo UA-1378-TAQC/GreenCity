5ba15cb35d358925fe684d4bc272da573bf50604
Dmytro Zubenko <fokist@outlook.com>
2025-05-24 17:09:25 +0300

    remainder added

--- green_city/tests/api/verify_disliking_eco_news_by_valid_id.py
+++ green_city/tests/api/verify_disliking_eco_news_by_valid_id.py
@@ -3,6 +3,7 @@ from green_city.src.config import API_BASE_URL_8085, ENDPOINTS
 
 def test_dislike_news(create_news,auth_token):
     news_id = create_news
+    #switch to another user here
     full_url = f"{API_BASE_URL_8085}{ENDPOINTS['dislike_eco_news'].format(news_id)}"
     headers = {
         "Authorization": auth_token
