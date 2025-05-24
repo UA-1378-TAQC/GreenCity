79ad1a2465f61e71b92f9c3b25fb8c0e0a7ec755
Dmytro Zubenko <fokist@outlook.com>
2025-05-24 17:52:14 +0300

    dislike news test corrected

--- green_city/tests/api/verify_disliking_eco_news_by_valid_id.py
+++ green_city/tests/api/verify_disliking_eco_news_by_valid_id.py
@@ -1,16 +1,14 @@
 import requests
 from green_city.src.config import API_BASE_URL_8085, ENDPOINTS
 
-def test_dislike_news(create_news,auth_token):
+def test_dislike_news(create_news,auth_token_second_user):
     news_id = create_news
-    #switch to another user here
     full_url = f"{API_BASE_URL_8085}{ENDPOINTS['dislike_eco_news'].format(news_id)}"
     headers = {
-        "Authorization": auth_token
+        "Authorization": auth_token_second_user
     }
 
     response = requests.post(full_url, headers=headers)
 
     assert response.status_code == 200, "Failed to dislike selected news"
-    data = response.json()
-    assert any(news.get("id") == news_id for news in data), f"News with ID {news_id} not found in response: {[n.get('id') for n in data]}"
+
