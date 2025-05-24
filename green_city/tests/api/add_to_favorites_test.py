efc93a36fa35bd1efc7707eea7a258f09769d236
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-05-25 02:13:14 +0300

    refactoring

--- green_city/tests/api/add_to_favorites_test.py
+++ green_city/tests/api/add_to_favorites_test.py
@@ -1,5 +1,4 @@
 import green_city.src.util.logging_config
-import pytest
 import requests
 from green_city.src.config import API_BASE_URL_8085, ENDPOINTS
 import logging
@@ -14,6 +13,12 @@ def request_add_to_favorites(token, id):
         headers={"Authorization": token}
       )
 
+def assert_test_response(response, expected_status_code, param, expected_response_by_param, schema_by_param):
+      assert response.status_code == expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}"
+      validate(instance=response.json(), schema=schema_by_param)
+      assert response.json()[param]==expected_response_by_param, f"Expected {param}: {expected_response_by_param} but got {response.json()[param]}"
+      
+
 def test_add_to_favorites_not_found(create_not_found_news, auth_token):
       id= create_not_found_news
       logger.info(f'Request to add new eco-new with id={id} to favorites')
@@ -23,7 +28,5 @@ def test_add_to_favorites_not_found(create_not_found_news, auth_token):
       expected_status_code = 404
       param="message"
       expected_response_by_param = f"Eco new doesn't exist by this id: {id}"
-
-      assert response.status_code == expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}"
-      validate(instance=response.json(), schema=single_message_schema)
-      assert response.json()[param]==expected_response_by_param, f"Expected {param}: {expected_response_by_param} but got {response.json()[param]}"
+      schema_by_param = single_message_schema
+      assert_test_response(response, expected_status_code, param, expected_response_by_param, schema_by_param)
