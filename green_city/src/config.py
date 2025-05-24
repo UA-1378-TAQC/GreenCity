d0ee45e94addcc43144d569619bcbdd6e8eb091e
Dmytro Zubenko <fokist@outlook.com>
2025-05-24 21:02:11 +0300

    endpoints added

--- green_city/src/config.py
+++ green_city/src/config.py
@@ -9,7 +9,9 @@ API_BASE_URL_8085 = os.getenv("API_BASE_URL_8085")
 ENDPOINTS = {
     "user_login": "/api/testers/sign-in",
     "news": "/eco-news/{0}",
-    "comments": "/eco-news/{0}/comments"
+    "comments": "/eco-news/{0}/comments",
+    "create_eco_news": "/eco-news",
+    "delete_eco_news":"/eco-news/{0}"
 }
 
 TEST_USER_EMAIL = os.getenv("TEST_USER_EMAIL")
