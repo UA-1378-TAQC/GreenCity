102b2b36c12d4a56b7ec0eddcd50e3b2632d41d6
Dmytro Zubenko <fokist@outlook.com>
2025-05-24 17:36:43 +0300

    reate news fixture corrected

--- green_city/tests/api/conftest.py
+++ green_city/tests/api/conftest.py
@@ -53,4 +53,11 @@ def create_news(auth_token):
 
     response = requests.post(url, headers=headers, files=files)
 
+    news_id = response.json().get("id")
+    yield news_id
+
+    delete_url = f"{API_BASE_URL_8085}{ENDPOINTS['delete_eco_news'].format(news_id)}"
+    del_response = requests.delete(delete_url, headers=headers)
+    assert del_response.status_code == 200, "Failed to delete news"
+
 
