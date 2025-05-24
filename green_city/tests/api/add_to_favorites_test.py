530a5e4019377c29b6986a09e9a57d71ec015e12
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-05-25 01:49:02 +0300

    implemantation of test_add_to_favorites_unauthorized

---
+++ green_city/tests/api/add_to_favorites_test.py
@@ -0,0 +1,30 @@
+import green_city.src.util.logging_config
+import pytest
+import requests
+from green_city.src.config import API_BASE_URL_8085, ENDPOINTS
+import logging
+from jsonschema import validate
+from ..data.schema.general_schemas import error_schema, single_message_schema
+
+logger = logging.getLogger(__name__)
+
+def request_add_to_favorites(token, id):
+      return requests.post(
+        f'{API_BASE_URL_8085}{ENDPOINTS["favorites"].format(id)}',
+        headers={"Authorization": token}
+      )
+
+def test_add_to_favorites_unauthorized(create_news):
+      id= create_news
+      logger.info(f'Request to add new eco-new with id={id} to favorites')
+      response = request_add_to_favorites('', id)
+      logger.info(f"Status code: {response.status_code}")
+      
+      expected_status_code = 401
+      param="error"
+      expected_response_by_param = "Unauthorized"
+
+      assert response.status_code == expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}"
+      validate(instance=response.json(), schema=error_schema)
+      assert response.json()[param]==expected_response_by_param, f"Expected {param}: {expected_response_by_param} but got {response.json()[param]}"
+    
