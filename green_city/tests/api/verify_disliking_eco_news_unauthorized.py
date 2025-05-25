7837ea7828d3c0c6c279cd8f06fcd5c50c00b3ef
Dmytro Zubenko <fokist@outlook.com>
2025-05-25 18:37:26 +0300

    test modified

--- green_city/tests/api/verify_disliking_eco_news_unauthorized.py
+++ green_city/tests/api/verify_disliking_eco_news_unauthorized.py
@@ -1,4 +1,6 @@
 def test_dislike_news_unauthorized(news_factory,dislike_news_factory):
     news_id = news_factory
     response = dislike_news_factory(news_id, auth_token=None)
+    json_body = response.json()
     assert response.status_code == 401
+    assert json_body["error"] == "Unauthorized"
