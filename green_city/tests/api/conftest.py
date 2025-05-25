6b583a72c4fcfffcaa262d6de1fe3de6581f01ce
Roman <fedkoroma2@gmail.com>
2025-05-25 23:23:20 +0300

    done

--- green_city/tests/api/conftest.py
+++ green_city/tests/api/conftest.py
@@ -1,2 +1,4 @@
 from green_city.tests.api.fixtures.login_fixture import auth_token, auth_token_second_user
 from green_city.tests.api.fixtures.create_news import create_news, create_not_found_news
+from green_city.tests.api.fixtures.create_event_fixtures import create_event
+from green_city.tests.api.fixtures.not_found_comments_fixtures import nonexistent_event_id
