e1cc405cc6b3bc774153311d231359f4d5cb6dfb
Dmytro Zubenko <fokist@outlook.com>
2025-05-25 18:02:59 +0300

    test added, fixture changed

--- green_city/tests/api/fixtures/dislike_news_fixture.py
+++ green_city/tests/api/fixtures/dislike_news_fixture.py
@@ -7,6 +7,5 @@ def dislike_news_factory():
     def _dislike(news_id, auth_token):
         full_url = f"{API_BASE_URL_8085}{ENDPOINTS['dislike_eco_news'].format(news_id)}"
         headers = {"Authorization": auth_token}
-        response = requests.post(full_url, headers=headers)
-        assert response.status_code == 200, "Failed to dislike news"
+        return requests.post(full_url, headers=headers)
     return _dislike
