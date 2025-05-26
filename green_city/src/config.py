3e2c98a29310470231a01ccc5709210366c30075
Khrystyna <kristina50963@gmail.com>
2025-05-26 14:58:53 +0300

    some update

--- green_city/src/config.py
+++ green_city/src/config.py
@@ -12,7 +12,7 @@ ENDPOINTS = {
     "comments": "/eco-news/{0}/comments",
     "create_eco_news": "/eco-news",
     "delete_eco_news":"/eco-news/{0}",
-    "get_eco_news": "/api/eco-news"
+    "get_eco_news": "/eco-news"
 }
 
 TEST_USER_EMAIL = os.getenv("TEST_USER_EMAIL")
--- green_city/tests/data/test_data/eco_news_invalid_page_values.py
+++ green_city/tests/data/test_data/eco_news_invalid_page_values.py
@@ -3,5 +3,5 @@ eco_news_invalid_page_values = [
     (-5, 400, "page must be a positive number"),
     ("abc", 400, "Invalid value for page: must be an integer"),
     (1.5, 400, "Invalid value for page: must be an integer"),
-    ("", 400, "page must be a positive number"),
+    ("", 400, "Invalid value for page: must be an integer"),
 ]
