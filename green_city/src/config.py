c465451e87d2909d4bb1559fbac0efda0c70dda7
KhrystynaTs <64132994+KhrystynaTs@users.noreply.github.com>
2025-05-28 23:09:55 +0300

    Update config.py

--- green_city/src/config.py
+++ green_city/src/config.py
@@ -12,12 +12,6 @@ ENDPOINTS = {
     "comments": "/eco-news/{0}/comments",
     "create_eco_news": "/eco-news",
     "delete_eco_news":"/eco-news/{0}",
-
-
-
-
-
-    
     "get_eco_news": "/eco-news"
 }
 
