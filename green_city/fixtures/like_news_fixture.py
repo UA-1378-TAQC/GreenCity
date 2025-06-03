9e0d12689e6cae854a6a2ae4eba5b02c9b50bbc3
Dmytro Zubenko <fokist@outlook.com>
2025-06-03 22:48:24 +0300

    small corrections

--- green_city/fixtures/like_news_fixture.py
+++ green_city/fixtures/like_news_fixture.py
@@ -1,6 +1,6 @@
 import pytest
 import requests
-from green_city.src.config import API_BASE_URL_8085, ENDPOINTS
+from green_city.config.config import API_BASE_URL_8085, ENDPOINTS
 
 @pytest.fixture(scope="function")
 def like_news(create_news, auth_token_second_user):
