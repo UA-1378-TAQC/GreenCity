6792f94977807c003e7a46e524d8eef54289bb6e
Dmytro Zubenko <fokist@outlook.com>
2025-06-03 22:44:16 +0300

    fixtures for test are back from ashes

--- green_city/fixtures/create_news_fixture.py
+++ green_city/fixtures/create_news_fixture.py
@@ -5,26 +5,5 @@ from green_city.fixtures.login_fixture import auth_token
 from green_city.tests.data.fixture_dto.create_news_dto import create_news_dto_request
 from green_city.config.config import API_BASE_URL_8085, ENDPOINTS
 
-@pytest.fixture(scope="function")
-def create_news(auth_token):
-    url = f"{API_BASE_URL_8085}{ENDPOINTS['create_eco_news']}"
 
-    dto_request_str = json.dumps(create_news_dto_request)
 
-    files = {
-        'addEcoNewsDtoRequest': (None, dto_request_str),
-        'image': (None, '')
-    }
-
-    headers = {
-        "Authorization": auth_token
-    }
-
-    response = requests.post(url, headers=headers, files=files)
-
-    news_id = response.json().get("id")
-    yield news_id
-
-    delete_url = f"{API_BASE_URL_8085}{ENDPOINTS['delete_eco_news'].format(news_id)}"
-    del_response = requests.delete(delete_url, headers=headers)
-    assert del_response.status_code == 200, "Failed to delete news"
